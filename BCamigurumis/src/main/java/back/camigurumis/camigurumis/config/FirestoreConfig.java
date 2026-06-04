9c12630025d25330f7d983c7ea00a681bea17600
CamilaGiraldoGiraldo <133153684+CamilaGiraldoGiraldo@users.noreply.github.com>
2026-06-04 15:16:00 -0500

    actualizacion

--- BCamigurumis/src/main/java/back/camigurumis/camigurumis/config/FirestoreConfig.java
+++ BCamigurumis/src/main/java/back/camigurumis/camigurumis/config/FirestoreConfig.java
@@ -1,10 +1,10 @@
 package back.camigurumis.camigurumis.config;
 
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.FirestoreOptions;
+import com.google.firebase.cloud.FirestoreClient;
 import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,35 +14,24 @@ import java.io.InputStream;
 @Configuration
 public class FirestoreConfig {
 
-    private Firestore firestore;
-
     @PostConstruct
-    public void init() {
-
+    public void initFirebase() {
         try {
-            // Cargar archivo desde resources
-            InputStream serviceAccount = getClass().getClassLoader()
-                    .getResourceAsStream("firebase.json");
-
-            if (serviceAccount == null) {
-                throw new RuntimeException("No se encontró firebase.json en resources");
-            }
-
-            FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .build();
-
             if (FirebaseApp.getApps().isEmpty()) {
+
+                InputStream serviceAccount = getClass().getClassLoader()
+                        .getResourceAsStream("firebase.json");
+
+                if (serviceAccount == null) {
+                    throw new RuntimeException("❌ No se encontró firebase.json en src/main/resources");
+                }
+
+                FirebaseOptions options = FirebaseOptions.builder()
+                        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                        .build();
+
                 FirebaseApp.initializeApp(options);
             }
-
-            this.firestore = FirestoreOptions.getDefaultInstance()
-                    .toBuilder()
-                    .setProjectId("camigurumis-82d23") // cambia si aplica
-                    .build()
-                    .getService();
-
-            System.out.println("✔ Firebase inicializado correctamente");
 
         } catch (Exception e) {
             throw new RuntimeException("Error al inicializar Firebase", e);
@@ -50,7 +39,7 @@ public class FirestoreConfig {
     }
 
     @Bean
-    public Firestore getFirestore() {
-        return firestore;
+    public Firestore firestore() {
+        return FirestoreClient.getFirestore();
     }
 }
