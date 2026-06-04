82e98a01fc6fc1182fb4cf60ce3209e98ccf1d35
CamilaGiraldoGiraldo <133153684+CamilaGiraldoGiraldo@users.noreply.github.com>
2026-06-04 14:54:46 -0500

    daño

--- BCamigurumis/src/main/java/back/camigurumis/camigurumis/config/FirestoreConfig.java
+++ BCamigurumis/src/main/java/back/camigurumis/camigurumis/config/FirestoreConfig.java
@@ -1,60 +1,33 @@
 package back.camigurumis.camigurumis.config;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.nio.charset.StandardCharsets;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 
 @Configuration
 public class FirestoreConfig {
 
     @Bean
-    public Firestore firestore() {
+    public Firestore firestore() throws IOException {
 
-        try {
+        if (FirebaseApp.getApps().isEmpty()) {
 
-            if (FirebaseApp.getApps().isEmpty()) {
+            FileInputStream serviceAccount = new FileInputStream("src/main/resources/serviceAccountKey.json");
 
-                GoogleCredentials credentials;
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
 
-                String firebaseJson = System.getenv("FIREBASE_CREDENTIALS_JSON");
-
-                if (firebaseJson != null && !firebaseJson.isBlank()) {
-
-                    credentials = GoogleCredentials.fromStream(
-                            new ByteArrayInputStream(
-                                    firebaseJson.getBytes(StandardCharsets.UTF_8)));
-
-                } else {
-
-                    File archivo = new File(
-                            "camigurumis-82d23-firebase-adminsdk-fbsvc-022284bddf.json");
-
-                    credentials = GoogleCredentials.fromStream(
-                            new FileInputStream(archivo));
-                }
-
-                FirebaseOptions options = FirebaseOptions.builder()
-                        .setCredentials(credentials)
-                        .setProjectId("camigurumis-82d23")
-                        .build();
-
-                FirebaseApp.initializeApp(options);
-            }
-
-            return FirestoreClient.getFirestore();
-
-        } catch (Exception e) {
-            throw new RuntimeException("Error inicializando Firestore", e);
+            FirebaseApp.initializeApp(options);
         }
+
+        return FirestoreClient.getFirestore();
     }
 }
