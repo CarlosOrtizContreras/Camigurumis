645efd7f62b0f3028c53f15dd1289ac3ea3e595c
CamilaGiraldoGiraldo <133153684+CamilaGiraldoGiraldo@users.noreply.github.com>
2026-06-04 20:16:53 -0500

    nuevo

--- BCamigurumis/src/main/java/back/camigurumis/camigurumis/config/FirestoreConfig.java
+++ BCamigurumis/src/main/java/back/camigurumis/camigurumis/config/FirestoreConfig.java
@@ -1,15 +1,18 @@
 package back.camigurumis.camigurumis.config;
 
+import java.io.ByteArrayInputStream;
+import java.util.Base64;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
-import jakarta.annotation.PostConstruct;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-import java.io.InputStream;
+import jakarta.annotation.PostConstruct;
 
 @Configuration
 public class FirestoreConfig {
@@ -17,17 +20,24 @@ public class FirestoreConfig {
     @PostConstruct
     public void initFirebase() {
         try {
+
             if (FirebaseApp.getApps().isEmpty()) {
 
-                InputStream serviceAccount = getClass().getClassLoader()
-                        .getResourceAsStream("firebase.json");
+                String base64 = System.getenv("FIREBASE_CREDENTIALS_BASE64");
 
-                if (serviceAccount == null) {
-                    throw new RuntimeException("❌ No se encontró firebase.json en src/main/resources");
+                if (base64 == null || base64.isBlank()) {
+                    throw new RuntimeException(
+                            "No existe FIREBASE_CREDENTIALS_BASE64");
                 }
 
+                byte[] decoded = Base64.getDecoder().decode(base64);
+
                 FirebaseOptions options = FirebaseOptions.builder()
-                        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                        .setCredentials(
+                                GoogleCredentials.fromStream(
+                                        new ByteArrayInputStream(decoded)
+                                )
+                        )
                         .build();
 
                 FirebaseApp.initializeApp(options);
